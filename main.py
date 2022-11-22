1ad218552b7e9dd33989537397666c76a2d41dff
fsdalpiaz <filipsdalpiaz@gmail.com>
2022-11-22 00:37:26 -0300

    Start interface

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from guizero import App, Text
+# creator @fsdalpíaz
+# using guizero package
 
-app = App("Wanted!")
+from guizero import *
 
-app.display()
+
+# janela da calculadora
+calcapp = App(title="Calculadora",
+                  width=400,
+                  height=500)
+
+# box display
+displaynumbers = Box(calcapp,
+                     width=398,
+                     height=100,
+                     border=3)
+
+#
+
+calcapp.display()
